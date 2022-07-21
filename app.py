94d082ec8acb63b21332cd47213bacd2fb6f73e7
Mousumi Sen <98817832+MousumiAria@users.noreply.github.com>
2022-07-21 20:39:59 +0200

    changes

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ from predict import prediction
 app = Flask(__name__)
  
 #route to form to get house details from user
-@app.route('/HouseDetails/', methods = ['POST', 'GET'])
+#@app.route('/HouseDetails/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST', 'GET'])
 def inputdata():
+
+    index_pqge="Default.html"
     if request.method == 'GET':
-        return render_template('HouseDetails.html')
+        return render_template(index_pqge)
     if request.method == 'POST':
 
         #check which button is clicked
@@ -19,19 +22,20 @@ def inputdata():
         #f validate then prepare the json data else predict the price
         if btn_action == "Validate":
             jsonReq = createJsonDataFromRequest(request)
-            return render_template('HouseDetails.html',json_data = jsonReq)
+            return render_template(index_pqge,json_data = jsonReq)
         else:
-            house_data = request.form['jsonHouse_data'] 
+            #house_data = request.form['jsonHouse_data'] 
+            house_data =  createJsonDataFromRequest(request)
 
             if house_data != "":
                 data = json.loads(house_data)                
                 #list = data
                 predic_price = prediction.predict(list(data.values()))
                 round_price=round(predic_price,2)
-                return render_template('HouseDetails.html',price = round_price)
+                return render_template(index_pqge,price = round_price)
             else:
                 error_data = "please validate first"
-                return render_template('HouseDetails.html',error = error_data)
+                return render_template(index_pqge,error = error_data)
           
         
 def createJsonDataFromRequest(req):
@@ -52,9 +56,23 @@ def createJsonDataFromRequest(req):
    
     data["house_surface"]=  int(form_data['house_surface'])
     data["house_bedroom"]=  int(form_data['house_bedroom'])
-    data["house_attic"]=  int(form_data['house_attic'])
-    data["house_terrace"]=  int(form_data['house_terrace'])
-    data["house_swimmingpool"]=  int(form_data['house_swimmingpool'])
+
+    if 'house_attic' in form_data:
+      data["house_attic"] =  int(form_data['house_attic'])
+    else:
+      data["house_attic"] =  0
+   
+    if 'house_terrace' in form_data:
+      data["house_terrace"] =  int(form_data['house_terrace'])
+    else:
+      data["house_terrace"] =  0
+   
+
+    if 'house_swimmingpool' in form_data:
+      data["house_swimmingpool"] =  int(form_data['house_swimmingpool'])
+    else:
+      data["house_swimmingpool"] =  0
+
     json_data = json.dumps(data)
 
     return json_data
